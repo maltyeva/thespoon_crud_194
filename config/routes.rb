a24fc7412d8f0137a82f17f20eadbc6efb43a409
Maria <maria.altyeva@gmail.com>
2018-11-13 09:19:59 +0800

    created restaurants model and restaurants routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #CRUD
+
+  #Read ALL restaurants
+  # get '/restaurants', to: "restaurants#index"
+
+  # #Read ONE restaurant
+  # # restaurants/34
+  # # params[:id] =>
+  # get '/restaurants/:id', to: "restaurants#show"
+
+  # #Create one restaurant
+  # # 1st request gets the form
+  # get '/restaurants/new', to: "restaurants#new"
+  # # 2nd saves it to the database
+  # post '/restaurants', to: "restaurants#create"
+
+  # #Update one restaurant
+  # #1st get the form for the request with the restaurants
+  # # /restauraunts/4/edit/
+  # get '/restaurants/:id/edit', to: "restaurants#edit"
+  # #2nd update/save to the database
+  # patch '/restaurants/:id', to: "restaurants#update"
+
+  # #Destroy/Delete one restaurant
+  # delete "/restaurants/:id", to: "restaurants#destroy"
+
+  resources :restaurants
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.delete_all
+
+10.times do
+  Restaurant.create!(name: Faker::Hipster.word, rating: Faker::Number.between(1, 5), address: Faker::Address.city)
+end
+
+puts "Created #{Restaurant.count} Restaurants!"
